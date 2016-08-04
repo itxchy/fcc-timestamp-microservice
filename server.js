8e1eb79b2b6d673ae65942f090c83a62ef572eaa
Matt Trifilo <matt.trifilo@gmail.com>
2016-08-04 12:11:00 -0500

    add natural date logic

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const compression = require('compression');
 const express     = require('express');
 const _           = require('lodash');
+const moment      = require('moment');
 const app         = express();
 
 app.use(compression());
@@ -11,25 +12,30 @@ app.use('/', express.static(__dirname + '/public'));
 app.get('/:time', (req, res) => {
 
     let time = req.params.time;
+    let timestamp = isUnixTimestamp(time);
     let naturalDate = null;
-    let unixTimestamp = null;
-    let timestampCheck = isUnixTimestamp(time);
-    let covertedTimes = {
-        unixTimeStamp: unixTimeStamp,
-        naturalDate: naturalDate
+    let convertedTimes = {
+        unix: null,
+        naturalDate: null
+    };
+
+    // if timestamp is true, and is greater than 0, proceed
+    if (timestamp > 0) {
+        convertedTimes.unix = timestamp;
+        convertedTimes.naturalDate = moment.unix(timestamp).format('MMMM Do YYYY');
+        res.json(convertedTimes);
     }
 
-    // if timestampCheck passes, and is greater than 0, proceed
-    if (timestampCheck > 0) {
-        unixTimestamp = timestampCheck;
-        naturalDate = moment(timestamp).format('MMMM Do YYYY');
-        res.json(convertedTimes);
+    naturalDate = validateDate(time);
+
+    // if a valid natural date was passed as a param, parse it.
+    if (naturalDate) {
+        convertedTimes.unix = moment(naturalDate, 'MMMM Do YYYY').unix();
+        convertedTimes.naturalDate = naturalDate;
     }
 
     res.end(`<h1>${time}</h1>`);
 });
-
-
 
 // returns a number or false
 function isUnixTimestamp (time) {
@@ -38,6 +44,17 @@ function isUnixTimestamp (time) {
 
     if (timeNumber !== NaN) {
         return timeNumber;
+    } else {
+        return false;
+    }
+}
+
+function validateDate (time) {
+
+    let checkDate = moment(time, 'MMMM Do YYYY').isValid;
+
+    if (checkDate) {
+        return time;
     } else {
         return false;
     }
