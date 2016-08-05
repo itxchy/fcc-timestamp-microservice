3bd1cf464e6e6f34b036abec5c9d93ee222c2ef0
Matt Trifilo <matt.trifilo@gmail.com>
2016-08-05 12:02:59 -0500

    Wrap verification functions in an IIFE

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/', express.static(__dirname + '/public'));
 app.get('/:time', (req, res) => {
 
     let time = req.params.time;
-    let timestamp = isUnixTimestamp(time);
+    let timestamp = timeHelpers.isUnixTimestamp(time);
     let naturalDate = null;
     let convertedTimes = {
         unix: null,
@@ -30,7 +30,7 @@ app.get('/:time', (req, res) => {
         res.json(convertedTimes);
     }
 
-    naturalDate = validateDate(time);
+    naturalDate = timeHelpers.validateDate(time);
 
     // if a valid natural date was passed as a param, parse it.
     if (naturalDate) {
@@ -44,35 +44,44 @@ app.get('/:time', (req, res) => {
     res.json(convertedTimes);
 });
 
-// returns a number or false
-function isUnixTimestamp (time) {
+var timeHelpers = (function () {
 
-    let timeNumber = _.toNumber(time);
+    // returns a number or false
+    function isUnixTimestamp (time) {
 
-    if (timeNumber !== NaN) {
-        return timeNumber;
-    } else {
-        return false;
+        let timeNumber = _.toNumber(time);
+
+        if (timeNumber !== NaN) {
+            return timeNumber;
+        } else {
+            return false;
+        }
     }
-}
 
-// returns a string of the valid date, or false
-function validateDate (time) {
+    // returns a string of the valid date, or false
+    function validateDate (time) {
 
-    let checkDate = moment(time, 'MMMM Do YYYY', true).isValid;
+        let checkDate = moment(time, 'MMMM Do YYYY', true).isValid;
 
-    let parsingFlagsObj = moment(time, 'MMMM Do YYYY', true).parsingFlags();
+        let parsingFlagsObj = moment(time, 'MMMM Do YYYY', true).parsingFlags();
 
-    /* if checkDate is true, and the invalidMonth property of .isValid's parsing flags is null, 
-    *  the date is valid. If invalidMonth's value is a mispelled month (or anything else), 
-    *  the date is invalid.
-    */
-    if (checkDate && !parsingFlagsObj.invalidMonth) {
-        return time;
-    } else {
-        return false;
+        /* if checkDate is true, and the invalidMonth property of .isValid's parsing flags is null, 
+        *  the date is valid. If invalidMonth's value is a mispelled month (or anything else), 
+        *  the date is invalid.
+        */
+        if (checkDate && !parsingFlagsObj.invalidMonth) {
+            return time;
+        } else {
+            return false;
+        }
     }
-}
+
+    return {
+        isUnixTimestamp: isUnixTimestamp,
+        validateDate: validateDate
+    };
+
+})();
 
 app.listen(4000, () => {
     console.log('Express app listening on port 4000!');
