79851c85f5f8a956bd5a854eaca04d8b40d2bc6d
Matt Trifilo <matt.trifilo@gmail.com>
2016-08-08 15:57:17 -0500

    clean up validateDate

--- server.js
+++ server.js
@@ -62,17 +62,9 @@ var timeHelpers = (function () {
     // returns a string of the valid date, or false
     function validateDate (time) {
 
-        let checkDate = moment(time, 'MMMM Do YYYY', true).isValid;
+        let checkDate = moment(time, 'MMMM Do YYYY', true).isValid();
 
-        let parsingFlagsObj = moment(time, 'MMMM Do YYYY', true).parsingFlags();
-
-        //console.log('parsing obj', parsingFlagsObj);
-
-        /** if checkDate is true, and the invalidMonth property of .isValid's parsing flags is null, 
-         *  the date is valid. If invalidMonth's value is a mispelled month (or anything else), 
-         *  the date is invalid.
-         */
-        if (checkDate && !parsingFlagsObj.invalidMonth) {
+        if (checkDate) {
             return time;
         } else {
             return false;
